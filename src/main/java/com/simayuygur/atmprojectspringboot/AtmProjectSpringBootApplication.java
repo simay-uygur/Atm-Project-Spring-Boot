474fd83012ca3a92eb37589e8fcb7df02550e9d5
simay-uygur <simayuygur09@gmail.com>
2024-08-27 20:13:29 +0300

    first commit

---
+++ src/main/java/com/simayuygur/atmprojectspringboot/AtmProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.simayuygur.atmprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmProjectSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmProjectSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simayuygur/atmprojectspringboot/AtmProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simayuygur.atmprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
